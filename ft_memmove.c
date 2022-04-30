5799a78bd92298f5d1ff5c2d4929350f25bc95df
JOKITON <jokinaizpuru28@gmail.com>
2022-04-30 17:27:34 +0200

    Makefile

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jaizpuru <jaizpuru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 16:20:09 by jaizpuru          #+#    #+#             */
-/*   Updated: 2022/04/07 16:38:41 by jaizpuru         ###   ########.fr       */
+/*   Updated: 2022/04/30 17:25:34 by jokiton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*content_src;
 	char	*content_dst;
-	size_t	loop;
 
-	loop = 0;
 	content_src = (char *)src;
 	content_dst = (char *)dst;
 	if (dst > src)
